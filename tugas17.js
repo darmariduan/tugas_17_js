2cc84ae37714ffcb29afa95c5db83293d75026a5
Darma Riduan <dharmarhiduwhan@gmail.com>
2020-01-20 12:06:51 +0700

    Tugas 17 Js

---
+++ tugas17.js
@@ -0,0 +1,15 @@
+function Ubah() {
+  var kata = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+  var kata2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+  console.log(kata.toLowerCase());
+  console.log(kata.toUpperCase());
+  console.log(kata2.toLowerCase());
+  console.log(kata2.toUpperCase());
+
+  console.log('');
+  console.log('');
+  console.log('charCodeAt');
+  var kata3 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+  console.log(kata3.charCodeAt(0),'-',kata3.charCodeAt(1),'-',kata3.charCodeAt(2),'-',kata3.charCodeAt(3),'-',kata3.charCodeAt(4),'-',kata3.charCodeAt(5));
+}
+Ubah();
